077e0e2c66baefc70ef4083a9a08e506562da236
Michael Pope <popemray1986@gmail.com>
2019-01-10 19:31:22 +0000

    Automatically backed up by Learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,23 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component {
+  state = {
+    reviews: []
+  }
+  
+  componentDidMount() {
+    fetch(`${URL}`)
+    .then( resp => resp.json())
+    .then( ({ results }) => this.setState({ reviews: results }))
+  }
+  
+  render() {
+    return (<div className="latest-movie-reviews">
+              <h2>Latest Movies:</h2>
+              < MovieReviews reviews= {this.state.reviews}/>
+            </div>)
+  }
+}
+
+export default LatestMovieReviewsContainer
